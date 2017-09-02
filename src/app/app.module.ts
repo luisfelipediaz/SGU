8aa265d6e76c9476783c8147a736f37257331769
luisfelipediaz <luis.felipe.diaz.v@gmail.com>
2017-09-01 20:30:46 -0500

    Se suben cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,21 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { TabsPage } from '../pages/tabs/tabs';
+
+import { CalendarioPage } from "../pages/calendario/calendario";
+import { RegistrarPage } from "../pages/registrar/registrar";
+import { NotificacionesPage } from "../pages/notificaciones/notificaciones";
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    CalendarioPage,
+    RegistrarPage,
+    NotificacionesPage,
     TabsPage
   ],
   imports: [
@@ -26,9 +26,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    CalendarioPage,
+    RegistrarPage,
+    NotificacionesPage,
     TabsPage
   ],
   providers: [
--- src/pages/registrar/registrar.ts
+++ src/pages/registrar/registrar.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+  selector: 'page-registrar',
+  templateUrl: 'registrar.html'
 })
-export class AboutPage {
+export class RegistrarPage {
 
   constructor(public navCtrl: NavController) {
-
   }
-
+  
 }
